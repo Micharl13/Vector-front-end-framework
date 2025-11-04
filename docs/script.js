1ae760bc9959c9fd48ecd866a473522ded3638b9
Micharl13 <110347385+Micharl13@users.noreply.github.com>
2025-11-04 12:08:43 -0500

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,1 +1,6 @@
-
+// Toggle dark/light theme for demo
+document.querySelectorAll(".vector-btn").forEach(btn => {
+  btn.addEventListener("click", () => {
+    alert("You clicked a Vector button!");
+  });
+});
